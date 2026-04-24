882de699d7eb320e0d62fbb1c52a7557749470b5
Veer Adyani <vwork108@gmail.com>
2026-04-24 16:44:44 +0530

    Revert "needed"
    This reverts commit 8e884cdabd689f1a8092ed15fcb412a7200cde12.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 
-console.log(import.meta.env.VITE_GOOGLE_CLIENT_ID);
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
