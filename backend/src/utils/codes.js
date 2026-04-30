5779056473f7707cc86bc0dfb3b4abd1e82e7eb6
Veer Adyani <vwork108@gmail.com>
2026-04-30 17:17:51 +0530

    Fix share key generator export

--- backend/src/utils/codes.js
+++ backend/src/utils/codes.js
@@ -2,13 +2,16 @@ const crypto = require('crypto');
 
 const ALPHABET = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
-function generateCode(length = 8) {
+function generateShareKey(length = 8) {
   const bytes = crypto.randomBytes(length);
   let out = '';
   for (let i = 0; i < length; i++) {
     out += ALPHABET[bytes[i] % ALPHABET.length];
   }
-  return out;
+  return out.toUpperCase();
 }
 
-module.exports = { generateCode };
+module.exports = {
+  generateShareKey,
+  generateCode: generateShareKey,
+};
